b093e98b28eb31804dea6e516390e96fc56919bb
David Blood <davidbjourno@users.noreply.github.com>
2016-07-30 16:49:06 +0100

    Add map; improve responsivity

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,2 +1,9 @@
+const mapboxgl = require('mapbox-gl');
 
-// YOUR CODE HERE
+mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWRiam91cm5vIiwiYSI6IjY2YmJkNDRkMGZhMjQ3ZDJkN2YxZDgyY2UyNjAyMjBjIn0.WQw7O86xdByVX0nwrmFLQw';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v9',
+  interactive: false,
+});
