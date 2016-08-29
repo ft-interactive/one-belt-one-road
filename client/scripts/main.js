fa9aee812f48d0e368269c5e7766837bf08796d7
David Blood <davidbjourno@users.noreply.github.com>
2016-08-29 15:24:46 +0100

    Rename central-asia-china-d ID

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -50,7 +50,7 @@ const locations = {
     pitch: 0,
     zoom: startZoom * 1.1,
   },
-  'central-asia-china-d': {
+  'central-asia-china-line-d': {
     center: [72.152674, 42.267858],
     bearing: 0,
     speed: 0.5,
